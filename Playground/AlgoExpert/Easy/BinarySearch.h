62bb4b4621642d1b3d1571ef6180ab4fbe9e07dd
EffortIsSucess <patrickreiner.schmelzer@gmail.com>
2019-11-26 19:42:50 +0100

    created project

---
+++ Playground/AlgoExpert/Easy/BinarySearch.h
@@ -0,0 +1,19 @@
+#ifndef ALGOEXPERT_BINARY_SEARCH
+#define ALGOEXPERT_BINARY_SEARCH
+
+#include <vector>
+
+class BinarySearch {
+public:
+	BinarySearch() = default;
+	~BinarySearch() = default;
+
+	//int binarySearch(std::vector<int> array, int target) {
+	//	return helper(array, target, 0, array.size() - 1);
+	//}
+
+private:
+
+};
+
+#endif // !ALGOEXPERT_BINARY_SEARCH
